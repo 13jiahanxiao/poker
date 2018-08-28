77c5b4bd33e383fa3892c2d4bb11fe5463c45931
13jiahanxiao <2565659862@qq.com>
2018-08-28 22:39:23 +0800

    if 5 card's suit is same import flush and write

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ClassLibrary4;
+using static System.Console;
 
 namespace ConsoleApp9
 {
@@ -13,14 +14,45 @@ namespace ConsoleApp9
         {
             Deck myDeck = new Deck();
             myDeck.Shuffle();
-            for (int i = 0; i < 52; i++)
+            bool Judge1 = true;
+            int flush = 0;
+            Card[] newCards = new Card[50];
+            for ( int i = 0; i < 50; i++)
             {
-                Card tempCard = myDeck.GetCard(i);
-                Console.Write(tempCard.ToString());
-                if (i != 51)
-                    Console.Write(",");
-                else
-                    Console.WriteLine();
+                newCards[i] = myDeck.GetCard(i);
+            }
+            for(int hand = 0; hand < 10; hand++)
+            {
+                bool Judge = true;
+                Judge1 = true;
+                Suit  newSuit= newCards[hand * 5].suit;
+                for(int i = 0; i < 5; i++)
+                {
+                    if(newSuit!=newCards[hand * 5 + i].suit)
+                    {
+                        Judge = false;
+                        Judge1 = false;
+                    }
+                }
+                if (Judge)
+                {
+                    flush = hand * 5;
+                    break;
+                }
+            }
+            if (Judge1)
+            {
+                WriteLine("flush");
+                for(int card = 0; card < 5; card++)
+                {
+                    Write(newCards[flush + card].suit);
+                    Write(newCards[flush + card].rank);
+                    WriteLine();
+                }
+            }
+            else
+            {
+                WriteLine("no flush");
             }
             Console.ReadKey();
         }
